74b7ae94a54d0b68363c76590d8ac9f207f372da
ssudev <goodys1011@naver.com>
2021-04-11 17:00:43 +0900

    컴포턴트 스캔과 의존관계 자동 주입 시작하기

--- core/src/test/java/hello/core/singleton/SingleTonTest.java
+++ core/src/test/java/hello/core/singleton/SingleTonTest.java
@@ -1,11 +1,10 @@
 package hello.core.singleton;
 
 import hello.core.AppConfig;
-import hello.core.member.Grade;
-import hello.core.member.Member;
-import hello.core.member.MemberService;
+import hello.core.member.*;
 import hello.core.order.Order;
 import hello.core.order.OrderService;
+import hello.core.order.OrderServiceImpl;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -45,4 +44,29 @@ public class SingleTonTest {
         Assertions.assertThat(memberService1).isSameAs(memberService2);
     }
 
+    @Test
+    @DisplayName("싱글톤 패턴 조회")
+    void springContainerCall() {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        MemberServiceImpl memberService = ac.getBean("memberService", MemberServiceImpl.class);
+        OrderServiceImpl orderService = ac.getBean("orderService", OrderServiceImpl.class);
+        MemberRepository memberRepository = ac.getBean("memberRepository", MemberRepository.class);
+
+        MemberRepository memberRepository1 = memberService.getMemberRepository();
+        MemberRepository memberRepository2 = orderService.getMemberRepository();
+
+        System.out.println("memberService -> memberRepository = " + memberRepository1);
+        System.out.println("orderService -> memberRepository = " + memberRepository2);
+        System.out.println("memberRepository = " + memberRepository);
+    }
+
+    @Test
+    void configurationDeep() {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+        AppConfig bean = ac.getBean(AppConfig.class);
+
+        System.out.println("bean.getClass() = " + bean.getClass());
+
+    }
 }
